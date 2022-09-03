ce0005fd7c4f66f8c3311e1c56000343f12f1571
LHS-11 <ghktjq1119@naver.com>
2022-09-03 21:43:51 +0900

    5. java study (String, StringBuilder(Buffer), LocalDate 등)

---
+++ JavaStudy/src/main/java/com/example/javastudy/java5/Run.java
@@ -0,0 +1,11 @@
+package com.example.javastudy.java5;
+
+public class Run {
+    public static void main(String[] args) {
+        String something = "This is a lot of text again";
+
+        for(int i=0;i<something.length();i++){
+            System.out.println(something.charAt(i));
+        }
+    }
+}
