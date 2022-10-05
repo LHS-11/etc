af0cdac1a5e15e8b7e6e07cab35f7795af5ec841
LHS-11 <ghktjq1119@naver.com>
2022-10-05 21:49:27 +0900

    jwt Filter 등록 테스트

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.cos.jwt.config;
 
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -17,7 +20,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.addFilterBefore(new MyFilter3(),BasicAuthenticationFilter.class); // SecurityFilterChain 중에 Filter 선택 가능
+        // 어떤 필터 전에 특정 필터를 걸때 사용
+        // 시큐리티 필터 체인이 내가 임의로 만든 필터보다 먼저 동작함
+        // 시큐리티 필터보다 먼저 필터가 걸리게 하려면 addFilterBefore 를 걸어야함
         http.csrf().disable();
+
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 내 서버는 stateless 세션 사용 X
                 .and()
