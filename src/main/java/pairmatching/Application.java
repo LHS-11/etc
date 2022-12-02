c2b0a2d98ae9e108607d8debc4778f2ce2cbd2d9
LHS-11 <ghktjq1119@naver.com>
2022-12-02 13:46:25 +0900

    [Feat] : 기능 선택 입력 기능 구현

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,6 +1,7 @@
 package pairmatching;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import view.InputView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,15 +9,7 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
-        List<String> crewList=new ArrayList<>();
-        crewList.add("a");
-        crewList.add("b");
-        crewList.add("c");
-        crewList.add("d");
-
-        List<String> shuffle = Randoms.shuffle(crewList);
-        for (String s : shuffle) {
-            System.out.println(s);
-        }
+        InputView inputView = new InputView();
+        inputView.selectFeature();
     }
 }
