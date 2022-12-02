8e2febcd9a7f040da8855576219dbea320e7fad2
LHS-11 <ghktjq1119@naver.com>
2022-12-02 14:13:01 +0900

    [Docs] : 파일 입출력 기능 추가

---
+++ src/main/java/domain/PairMatchingFileReader.java
@@ -0,0 +1,13 @@
+package domain;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PairMatchingFileReader {
+
+
+}
--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,17 +1,18 @@
 package pairmatching;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import domain.PairMatchingFileReader;
 import view.InputView;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         // TODO 구현 진행
-        InputView inputView = new InputView();
-        inputView.selectFeature();
-        inputView.selectPairMatching();
-        inputView.selectPairMatchingRetry();
+
+
     }
 }
