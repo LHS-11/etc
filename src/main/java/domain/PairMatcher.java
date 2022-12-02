30d5742a9a832d13aa9b8af93d42ef4afe4b4172
LHS-11 <ghktjq1119@naver.com>
2022-12-02 15:14:59 +0900

    [Feat] : 크루 크기에 따른 페어 매칭 기능 구현

---
+++ src/main/java/domain/PairMatcher.java
@@ -0,0 +1,41 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PairMatcher {
+
+    public List<List<String>> matchPairs(List<String> shuffledCrew) {
+        List<List<String>> pairGroup = new ArrayList<>();
+        if (checkGroupSizeEven(shuffledCrew)) {
+            matchPairsEvenSize(shuffledCrew, pairGroup);
+        }
+        if (!checkGroupSizeEven(shuffledCrew)) {
+            matchPairsEvenSize(shuffledCrew, pairGroup);
+            matchLastCrew(shuffledCrew, pairGroup);
+        }
+        return pairGroup;
+    }
+
+    private static void matchPairsEvenSize(List<String> shuffledCrew, List<List<String>> pairGroup) {
+        for (int i = 0; i < shuffledCrew.size(); i++) {
+            List<String> pair = new ArrayList<>();
+            for (int j = i; j < i + 2; j++) {
+                pair.add(shuffledCrew.get(i));
+            }
+            pairGroup.add(pair);
+        }
+    }
+
+    private static void matchLastCrew(List<String> shuffledCrew, List<List<String>> pairGroup) {
+        List<String> lastPair = pairGroup.get(pairGroup.size() - 1);
+        lastPair.add(shuffledCrew.get(shuffledCrew.size() - 1));
+        pairGroup.set(pairGroup.size() - 1, lastPair);
+    }
+
+    public boolean checkGroupSizeEven(List<String> group) {
+        return group.size() % 2 == 0;
+    }
+
+
+}
