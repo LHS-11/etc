fedac3bf40a55e51b661ae3fcbcda45852fcde4c
LHS-11 <ghktjq1119@naver.com>
2022-12-02 15:00:22 +0900

    [Docs] : 기능 목록 구현 추가

---
+++ src/main/java/domain/CrewPairMatchingGroup.java
@@ -0,0 +1,17 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CrewPairMatchingGroup {
+
+    private List<CrewPairMatchingInfo> crewPairMatchingInfos;
+
+    public CrewPairMatchingGroup(){
+        crewPairMatchingInfos = new ArrayList<>();
+    }
+
+    public void addCrewPairMatingInfo(CrewPairMatchingInfo crewPairMatchingInfo){
+        crewPairMatchingInfos.add(crewPairMatchingInfo);
+    }
+}
---
+++ src/main/java/domain/CrewPairMatchingInfo.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.List;
+
+public class CrewPairMatchingInfo {
+
+    private Course course;
+    private Level level;
+    private List<List<String>> pairMatchingInfo;
+
+
+}
