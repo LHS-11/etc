7071a96b1cbf1679a618d49072ae786cabea82a9
LHS-11 <ghktjq1119@naver.com>
2022-12-15 17:23:04 +0900

    feat(subway) : 출발역 입력 기능 구현

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -5,9 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String COMMAND_INPUT_VIEW_MESSAGE = "## 원하는 기능을 선택하세요.";
+    private static final String START_STATION_INPUT_VIEW_MESSAGE = "## 출발역을 입력하세요.";
     public String inputMainCommand(){
         System.out.println(COMMAND_INPUT_VIEW_MESSAGE);
         return Console.readLine();
+    }
+
+    public String inputStartStation(){
+        System.out.println(START_STATION_INPUT_VIEW_MESSAGE);
+        return inputStartStation();
     }
 
 
