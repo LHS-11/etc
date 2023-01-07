1ffb2cc4bce38af4cd55617ec1af716bb97558f0
HwaSup <ghktjq1119@naver.com>
2023-01-08 00:53:24 +0900

    Initial commit

---
+++ src/main/java/org/example/ArithmeticOperator.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.util.Arrays;
+
+public enum ArithmeticOperator {
+    ADDITION("+") {
+        @Override
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 + operand2;
+        }
+    },
+    SUBTRACTION("-") {
+        @Override
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 - operand2;
+        }
+    },
+    SUPPLY("*") {
+        @Override
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 * operand2;
+        }
+    },
+    DIVISION("/") {
+        @Override
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 / operand2;
+        }
+    };
+
+    private String operation;
+
+    ArithmeticOperator(String operation) {
+        this.operation = operation;
+    }
+
+    public static int calculate(int operand1, String operation, int operand2) {
+        return from(operation).arithmeticCalculate(operand1, operand2);
+    }
+
+    public static ArithmeticOperator from(String other) {
+        return Arrays.stream(ArithmeticOperator.values())
+                .filter(o -> o.operation.equals(other))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다"));
+    }
+
+    public abstract int arithmeticCalculate(final int operand1, final int operand2);
+}
---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.*;
+
+
+/**
+ * 요구사항
+ * 간단한 사칙연산을 할 수 있다.
+ * 양수로만 계산할 수 있다.
+ * 나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.
+ * MVC 패턴 기반으로 구현한다.
+ */
+public class CalculatorTest {
+
+    @DisplayName("덧셈 연산을 수행한다.")
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void additionTest(int operand1,String operation,int operand2,int expectedResult) {
+        //given
+
+        //when
+        int result = Calculator.calculate(new PositiveNumber(operand1), operation,new PositiveNumber(operand2));
+
+        //then
+        assertThat(result).isEqualTo(expectedResult);
+
+    }
+
+    private static Stream<Arguments> formulaAndResult(){
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(4, "*", 2, 8),
+                arguments(4, "/", 2, 2)
+        );
+    }
+
+    @DisplayName("0으로 나누면 예외를 발생시킨다.")
+    @Test
+    void createExceptionDivisionZero() {
+        assertThatCode(() -> Calculator.calculate(new PositiveNumber(10), "/", new PositiveNumber(0)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("0으로 나눌 수 없습니다.");
+    }
+}
