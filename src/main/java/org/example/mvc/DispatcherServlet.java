e73947144804d0bb243d23d4a4639a255860ab5f
HwaSup <ghktjq1119@naver.com>
2023-01-09 17:57:16 +0900

    feat : mvc 패턴 구현

--- src/main/java/org/example/mvc/DispatcherServlet.java
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -1,16 +1,23 @@
 package org.example.mvc;
 
 import org.example.mvc.controller.Controller;
+import org.example.mvc.controller.RequestMethod;
+import org.example.mvc.view.JspViewResolver;
+import org.example.mvc.view.ModelAndView;
+import org.example.mvc.view.View;
+import org.example.mvc.view.ViewResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 @WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
@@ -19,23 +26,44 @@ public class DispatcherServlet extends HttpServlet {
 
     private RequestMappingHandlerMapping requestMappingHandlerMapping;
 
+    private List<ViewResolver> viewResolvers;
+
+    private List<HandlerAdapter> handlerAdapters;
+
     @Override
     public void init() throws ServletException {
         requestMappingHandlerMapping = new RequestMappingHandlerMapping();
         requestMappingHandlerMapping.init();
+
+        handlerAdapters = List.of(new SimpleControllerHandlerAdapter());
+        viewResolvers = Collections.singletonList(new JspViewResolver());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.info("DispatcherServlet#service");
 
-
         try {
-            Controller handler = requestMappingHandlerMapping.findHandler(request.getRequestURI());
-            String viewName = handler.handleRequest(request, response);
+            // 핸들러매핑에서 핸들러를 찾음
+            Controller handler = requestMappingHandlerMapping
+                    .findHandler(new HandlerKey(RequestMethod.valueOf(request.getMethod()), request.getRequestURI()));
 
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
-            requestDispatcher.forward(request,response);
+            // 핸들러어댑터에서 핸들러를 실행 (컨트롤러 실행)
+            HandlerAdapter handlerAdapter = handlerAdapters.stream()
+                    .filter(h -> h.supports(handler))
+                    .findFirst()
+                    .orElseThrow(() -> new ServletException("No adapter for handler"));
+
+            // ModelAndView 반환
+            ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
+
+            // ViewResolver 에서 View 선택
+            for (ViewResolver viewResolver : viewResolvers) {
+                View view = viewResolver.resolveView(modelAndView.getViewName());
+                view.render(modelAndView.getModel(), request, response);
+            }
+
+
         } catch (Exception e) {
             log.error("exception occurred: [{}]", e.getMessage(), e);
         }
--- src/main/java/org/example/mvc/controller/ForwardController.java
+++ src/main/java/org/example/mvc/controller/ForwardController.java
@@ -3,10 +3,16 @@ package org.example.mvc.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HomeController implements Controller{
+public class ForwardController implements Controller{
+
+    private final String uriPath;
+
+    public ForwardController(String uriPath) {
+        this.uriPath = uriPath;
+    }
 
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return "home";
+        return uriPath;
     }
 }
