068ebeaa5e955af2110152695fd4840d738d2a70
HwaSup <ghktjq1119@naver.com>
2023-01-12 23:50:07 +0900

    #11 - jpa test 작성
    보려고 작성
    테스트 데이터와 관련이 있기 때문에, 이대로는 실패함

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
-// @Repository 굳이 안붙여도 됨. JpaRepository 상위클래스에서 이미 어노테이션이 붙어 있음 
+// @Repository 굳이 안붙여도 됨. JpaRepository 상위클래스에서 이미 어노테이션이 붙어 있음
 public interface ArticleRepository extends JpaRepository<Article, Long > {
 
 }
---
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,93 @@
+package com.fastcampus.projectboard.repository;
+
+import com.fastcampus.projectboard.config.JpaConfig;
+import com.fastcampus.projectboard.domain.Article;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+//@ActiveProfiles("testdb")
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+
+//    @Autowired
+    private ArticleRepository articleRepository;
+
+//    @Autowired
+    private ArticleCommentRepository articleCommentRepository;
+
+
+    public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,@Autowired ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine(){
+        //given
+
+        //when
+        List<Article> articles = articleRepository.findAll();
+
+        //then
+        assertThat(articles).isNotNull().hasSize(1);
+    }
+
+
+    @Test
+    void givenTestData_whenInserting_thenWorksFine(){
+        //given
+        long previousCount = articleRepository.count();
+        Article article = Article.of("new article", "new content", "#spring");
+
+        //when
+        Article savedArticle = articleRepository.save(article);
+
+        //then
+        assertThat(articleRepository.count()).isEqualTo(previousCount+1);
+    }
+
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine(){
+        //given
+        String updatedHashtag = "#springboot";
+        Article article = articleRepository.findById(1L).orElseThrow();
+        article.setHashtag(updatedHashtag);
+
+        //when
+        Article updatedArticle = articleRepository.save(article);
+
+        //then
+        assertThat(updatedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
+    }
+
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine(){
+        //given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        int deletedCommentSize = article.getArticleComments().size();
+
+        //when
+        articleRepository.delete(article);
+
+        //then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentSize);
+    }
+
+
+}
