bd58bfe16c0755b744e05a4a509a09e3ec314fa8
HwaSup <ghktjq1119@naver.com>
2023-01-13 02:02:44 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExecutor`,
    `QuerydslBinderCustomizer` 를 이용하여 간단하게 개발

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -1,13 +1,33 @@
 package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 
 @RepositoryRestResource
 // @Repository 굳이 안붙여도 됨. JpaRepository 상위클래스에서 이미 어노테이션이 붙어 있음
-public interface ArticleRepository extends JpaRepository<Article, Long > {
-
+public interface ArticleRepository extends
+        JpaRepository<Article, Long >,
+        QuerydslPredicateExecutor<Article>, // Article 안에 있는 모든 필드에 대한 기본 검사를 추가해줌
+        QuerydslBinderCustomizer<QArticle>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title,root.content ,root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);// like '%${}%'
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);// like '%${}%'
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);// like '%${}%'
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);// like '%${}%'
+//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '' -> 사용자가 % 를 추가해줘야함
+    }
 }
