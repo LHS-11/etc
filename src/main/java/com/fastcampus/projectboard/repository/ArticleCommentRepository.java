26849d68f7073a1c9540a27defd022829d02304f
HwaSup <ghktjq1119@naver.com>
2023-01-13 01:26:15 +0900

    #1 - Spring Data REST 테스트 제외 처리
    data rest 기능으로 통합테스트이기 때문에 무겁다. db 에도 영향을 주기 때문에 제외 처리함

--- src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource
-
 @Repository
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
 }
