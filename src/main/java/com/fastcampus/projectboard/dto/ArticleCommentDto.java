e193f2de93385745d2aebf721a106b5c2a1c0f8e
HwaSup <ghktjq1119@naver.com>
2023-01-15 17:54:46 +0900

    #20 - 댓글 서비스 로직의 테스트와 골격 잡기

---
+++ src/main/java/com/fastcampus/projectboard/dto/ArticleCommentDto.java
@@ -0,0 +1,17 @@
+package com.fastcampus.projectboard.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public record ArticleCommentDto(
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy,
+        String content
+) implements Serializable {
+
+    public static ArticleCommentDto of(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String content) {
+        return new ArticleCommentDto(createdAt, createdBy, modifiedAt, modifiedBy, content);
+    }
+}
