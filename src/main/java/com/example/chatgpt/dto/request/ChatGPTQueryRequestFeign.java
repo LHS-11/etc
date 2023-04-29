0755063177a8d4cdb207134a7455036c51b8f701
HwaSup <ghktjq1119@naver.com>
2023-04-29 21:21:14 +0900

    rename : Feign 을 이용한 질의응답 Request 클래스 의도가 명확해지도록 ChatGPTRequest -> ChatGPTQueryRequestFeign 클래스명 변경

--- src/main/java/com/example/chatgpt/dto/request/ChatGPTQueryRequestFeign.java
+++ src/main/java/com/example/chatgpt/dto/request/ChatGPTQueryRequestFeign.java
@@ -7,21 +7,21 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class ChatGPTRequest {
+public class ChatGPTQueryRequestFeign {
 
     private String model;
     private String prompt;
     private int temperature;
     private int max_tokens;
 
-    private ChatGPTRequest(String model, String prompt, int temperature, int max_tokens) {
+    private ChatGPTQueryRequestFeign(String model, String prompt, int temperature, int max_tokens) {
         this.model = model;
         this.prompt = prompt;
         this.temperature = temperature;
         this.max_tokens = max_tokens;
     }
 
-    public static ChatGPTRequest from(ChatGPTQueryRequest chatGPTQueryRequest){
-        return new ChatGPTRequest(chatGPTQueryRequest.getModel(), chatGPTQueryRequest.getPrompt(), chatGPTQueryRequest.getTemperature(), chatGPTQueryRequest.getMax_tokens());
+    public static ChatGPTQueryRequestFeign from(ChatGPTQueryRequest chatGPTQueryRequest){
+        return new ChatGPTQueryRequestFeign(chatGPTQueryRequest.getModel(), chatGPTQueryRequest.getPrompt(), chatGPTQueryRequest.getTemperature(), chatGPTQueryRequest.getMax_tokens());
     }
 }
